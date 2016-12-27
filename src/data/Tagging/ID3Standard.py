f72dbcae6c9e3f9f42160b1ab2d8bc5184de506d
Noah Hummel <strange.dev@gmail.com>
2016-12-27 03:29:16 +0100

    Updated ID3 tag definitions

--- src/data/Tagging/ID3Standard.py
+++ src/data/Tagging/ID3Standard.py
@@ -1,7 +1,33 @@
 from typing import Dict
 from typing import List
 
-ID3_ATTRIBUTES = {
+ID3_ATTRIBUTES = [
+    "TPE1",  # type: str
+    "TALB",  # type: str
+    "TIT2",  # type: str
+    "TIT3",  # type: str
+    "TPE2",  # type: str
+    "TPE3",  # type: str
+    "TPE4",  # type: str
+    "TCOM",  # type: str
+    "TEXT",  # type: str
+    "IPLS",  # type: List[str]
+    "TRCK",  # type: str
+    "TPUB",  # type: str
+    "TCON",  # type: List[str]
+    "TYER",  # type: str
+    "TBPM",  # type: str
+    "TKEY",  # type: str
+    "TMOO",  # type: str
+    "TLEN",  # type: str
+    "ULST",  # type: str
+    "WOAR",  # type: str
+    "WPUB",  # type: str
+    "TFLT",  # type: str
+    "COMM"  # type: str
+]
+
+ID3_DISPLAY_NAMES = {
     "TPE1": "Artist",  # type: str
     "TALB": "Album",  # type: str,
     "TIT2": "Title",  # type: str
@@ -26,3 +52,4 @@ ID3_ATTRIBUTES = {
     "TFLT": "File type",  # type: str
     "COMM": "User comment",  # type: str
 }
+
