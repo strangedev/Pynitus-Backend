57a2b7424afa220649b45156054df52ce6fd0046
Vivian <vivian.franz4@gmail.com>
2017-01-04 04:07:12 +0100

    Add TagReader unimplemented

--- src/data/Tagging/TagReader.py
+++ src/data/Tagging/TagReader.py
@@ -1,6 +1,19 @@
 import taglib
+from typing import Dict
 
 
-class ID3TagReader(object):
+class TagReader(object):
     def __init__(self, file_path: str) -> None:
         self.audio_file = taglib.File(file_path)
+
+    def readTag(self) -> Dict[str, any]:
+        pass
+
+    def writeTag(self, key: str, **kwargs) -> None:
+        """
+
+        :param key: key to Change
+        :param kwargs: value of Key
+        :return: None
+        """
+        pass
