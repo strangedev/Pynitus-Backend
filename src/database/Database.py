9918c0637110f0595b0aa1c313177f102bf0ed21
Noah Hummel <strange.dev@gmail.com>
2017-01-04 01:56:03 +0100

    Implemented some methods in Database

--- src/database/Database.py
+++ src/database/Database.py
@@ -1,41 +1,59 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from src.data.Track.Track import Track
+from src.data.Track.TrackFactory import TrackFactory
+from src.database.IDatabaseAdapter import IDatabaseAdapter
 
 
 class Database(object):
+
+    def __init__(self, database: IDatabaseAdapter, track_factory: TrackFactory):
+        self.db = database
+        self.trackFactory = track_factory
 
     def refreshDB(self) -> None:
         return NotImplemented
 
     def addTrack(
             self,
-            title: str,
-            artist: str,
-            album: str,
-            type: str,
             location: str,
+            title: str=None,
+            artist: str=None,
+            album: str=None,
+            track_type: str=None,
             **kwargs
     ) -> None:
-        return NotImplemented
+        self.db.addTrack(location, title, artist, album, track_type, **kwargs)
 
-    def getTracksByArtist(self, artist: str) -> [Track]:
-        return NotImplemented
+    def getTracks(self) -> List[Track]:
+        track_dicts = self.db.getTracks()
+        return [self.trackFactory.getTrack(**track_dict) for track_dict in track_dicts]
 
-    def getTracksByAlbum(self,
-                         artist: str,
-                         album: str) -> [Track]:
-        return NotImplemented
+    def getArtists(self) -> List[str]:
+        return self.db.getArtists()
 
-    def getTrack(self,
-                 title: str,
-                 artist: str,
-                 album: str) -> Track:
-        return NotImplemented
+    def getAlbums(self) -> List[str]:
+        return self.db.getAlbums()
 
-    def getUnavailable(self) -> [Track]:
-        return NotImplemented
+    def getAlbumsByArtist(self, artist: str) -> List[str]:
+        return self.db.getAlbumsByArtist(artist)
 
-    def getUnimported(self) -> [Track]:
-        return NotImplemented
+    def getTracksByArtist(self, artist: str) -> List[Track]:
+        track_dicts = self.db.getTracksByArtist(artist)
+        return [self.trackFactory.getTrack(**track_dict) for track_dict in track_dicts]
+
+    def getTracksByAlbum(self, artist: str, album: str) -> List[Track]:
+        track_dicts = self.db.getTracksByAlbum(artist, album)
+        return [self.trackFactory.getTrack(**track_dict) for track_dict in track_dicts]
+
+    def getTrack(self, title: str, artist: str, album: str) -> Track:
+        return self.trackFactory.getTrack(**self.db.getTrack(title, artist, album))
+
+    def getUnimported(self) -> List[Track]:
+        track_dicts = self.db.getUnimported()
+        return [self.trackFactory.getTrack(**track_dict) for track_dict in track_dicts]
+
+    def getUnavailable(self) -> List[Track]:
+        track_dicts = self.db.getUnavailable()
+        return [self.trackFactory.getTrack(**track_dict) for track_dict in track_dicts]
 
