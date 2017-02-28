5c25aead099c7cd429e2a0b5a99d5f0de14d13cc
Noah Hummel <strange.dev@gmail.com>
2017-02-28 20:45:26 +0100

    Added method to automatically insert random tracks into DB

---
+++ Pynitus/test/db/sample_data.py
@@ -0,0 +1,30 @@
+import os
+
+from Pynitus import init_db
+from Pynitus.model import tracks
+from Pynitus.model.db.database import db_session, persistance
+
+
+def generate_sample_tracks(n: int=10000):
+
+    import Pynitus.model.db.database
+    init_db()
+
+    for i in range(n):
+
+        random_title = os.urandom(32).hex()
+        random_album_title = os.urandom(32).hex()
+        random_artist_name = os.urandom(32).hex()
+
+        t = tracks.get_or_create(random_title, random_artist_name, random_album_title)
+
+        with persistance():
+            t.status.available = True
+            t.status.imported = True
+
+    db_session.close()
+    db_session.remove()
+
+
+if __name__ == "__main__":
+    generate_sample_tracks(1000)
